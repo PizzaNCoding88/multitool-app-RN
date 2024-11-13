0bddcd47aceb311b1de93eaf85ccc986ba0d1ecb
PizzaNCoding88 <umbertonardiello@gmail.com>
2024-11-13 17:32:32 +0000

    tried to change fonts

--- components/Task.jsx
+++ components/Task.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, View } from "react-native";
 import styles from "../styles/components/task";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
 
