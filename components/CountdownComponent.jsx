ce9104d64ed1826b9ef329f93a2967421ee98e12
PizzaNCoding88 <umbertonardiello@gmail.com>
2024-11-19 16:50:00 +0000

    timer tests

--- components/CountdownComponent.jsx
+++ components/CountdownComponent.jsx
@@ -1,6 +1,7 @@
-import { TouchableOpacity } from "react-native-web";
+import { TouchableOpacity, Text, Alert, Pressable, View } from "react-native";
 import { useCountdown } from "rn-countdown-manager";
 import { Countdown } from "rn-countdown-manager";
+
 import {
   CustomBlocksType, // component custom assets
   ZERO_TO_FIFTY_NINE, // used on minutes and seconds
@@ -25,33 +26,77 @@ export default function CountdownComponent() {
   } = useCountdown({
     autoStart: false, // tells if hook stats on mount
     hours: 1, // starting hours (Max to 99)
-    minutes: 2, // starting minutes (Max to 59)
-    seconds: 3, // starting seconds (Max to 59)
-    onComplete: () => console.log("Finished!"), // callback when hook finishes
+    minutes: 0, // starting minutes (Max to 59)
+    seconds: 10, // starting seconds (Max to 59)
+    onComplete: () => console.log("Finished!"),
   });
 
-  function startStop() {
-    if (start) {
-      pause;
+  const startTimer = () => {
+    if (isRunning) {
+      pause();
     } else {
-      start;
+      start();
     }
-  }
+  };
+
+  const pauseTimer = () => {
+    pause();
+  };
 
   return (
-    <TouchableOpacity key="1" onPress={() => startStop()}>
-      <Countdown
-        hours={hours} // hours on component (Max to 99)
-        seconds={seconds} // minutes on component (Max to 59)
-        minutes={minutes} // seconds on component (Max to 59)
-        showHours={true} // if hours digits should be shown
-        showMinutes={true} // if minutes digits should be shown
-        color="#03DAC5" // cell color when on (defaults to #000000)
-        // offColor="#03DAC5" // cell color when off (defaults to transparent)
-        size="lg" // component size, can be "sm", "md" or "lg"
-        // customBlocks={customBlockAssetsJson} // custom SVGs can be used in cells
-        // CustomSeparator={() => <Text> - </Text>} // custom separator for HH:MM:SS, accepts any React.FC
-      />
-    </TouchableOpacity>
+    <>
+      <View style={{ gap: 50 }}>
+        <Countdown
+          hours={hours} // hours on component (Max to 99)
+          seconds={seconds} // minutes on component (Max to 59)
+          minutes={minutes} // seconds on component (Max to 59)
+          showHours={true} // if hours digits should be shown
+          showMinutes={true} // if minutes digits should be shown
+          color="#03DAC5" // cell color when on (defaults to #000000)
+          // offColor="#03DAC5" // cell color when off (defaults to transparent)
+          size="lg" // component size, can be "sm", "md" or "lg"
+          // customBlocks={customBlockAssetsJson} // custom SVGs can be used in cells
+          // CustomSeparator={() => <Text> - </Text>} // custom separator for HH:MM:SS, accepts any React.FC
+        />
+
+        <View style={{ alignItems: "center" }}>
+          <Pressable
+            onPress={() => startTimer()}
+            style={{
+              borderColor: "white",
+              borderWidth: 2,
+              paddingVertical: 15,
+              paddingHorizontal: 15,
+              borderRadius: 20,
+              borderColor: "#03DAC5",
+            }}
+          >
+            <Text style={{ color: "#03DAC5", fontFamily: "monospace" }}>
+              Start/Pause Timer
+            </Text>
+          </Pressable>
+        </View>
+        <View style={{ alignItems: "center" }}>
+          <Pressable
+            onPress={() => reset()}
+            style={{
+              borderColor: "white",
+              borderWidth: 2,
+              paddingVertical: 15,
+              paddingHorizontal: 15,
+              borderRadius: 20,
+              borderColor: "#03DAC5",
+            }}
+          >
+            <Text style={{ color: "#03DAC5", fontFamily: "monospace" }}>
+              Reset Timer
+            </Text>
+          </Pressable>
+        </View>
+        {/* <Pressable onPress={() => pauseTimer()}>
+          <Text style={{ color: "white" }}>Pause</Text>
+        </Pressable> */}
+      </View>
+    </>
   );
 }
