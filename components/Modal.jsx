c27b71ab75351c4d8f84885bbbfa5dfe698b2bf4
PizzaNCoding88 <umbertonardiello@gmail.com>
2024-11-22 17:06:24 +0000

    modal styling

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -3,6 +3,8 @@
 import { View, TextInput, Pressable, Text } from "react-native";
 import React, { useState } from "react";
 
+import styles from "../styles/components/modal";
+
 const Modal = ({ showModal, handleData, showCountdown }) => {
   const [mins, setMins] = useState("");
   const [secs, setSecs] = useState("");
@@ -14,36 +16,35 @@ const Modal = ({ showModal, handleData, showCountdown }) => {
   };
 
   return (
-    <View style={{ flex: 1, justifyContent: "center" }}>
-      <Text style={{ color: "white" }}>Modal</Text>
-      <View style={{ gap: 20 }}>
-        <TextInput
-          keyboardType="numeric"
-          style={{ backgroundColor: "white", width: 300, height: 35 }}
-          placeholder="Mins"
-          value={mins}
-          onChangeText={(mins) => setMins(mins)}
-        />
-        <TextInput
-          keyboardType="numeric"
-          style={{ backgroundColor: "white", width: 300, height: 35 }}
-          placeholder="Secs"
-          value={secs}
-          onChangeText={(seconds) => setSecs(seconds)}
-        />
-        <Pressable
-          style={{
-            borderColor: "#03DAC5",
-            borderWidth: 2,
-            width: 60,
-            alignItems: "center",
-          }}
-          onPress={submit}
-        >
-          <Text style={{ color: "white" }}>Set</Text>
+    <>
+      <View style={styles.overlay}></View>
+      <View style={styles.container}>
+        <Text style={styles.title}>ENTER TIME</Text>
+        <View style={styles.selectorContainer}>
+          <View style={styles.minutesSelectorContainer}>
+            <TextInput
+              keyboardType="numeric"
+              style={styles.timeInput}
+              value={mins}
+              onChangeText={(mins) => setMins(mins)}
+            />
+            <Text style={styles.labelText}>Minutes</Text>
+          </View>
+          <View style={styles.minutesSelectorContainer}>
+            <TextInput
+              keyboardType="numeric"
+              style={styles.timeInput}
+              value={secs}
+              onChangeText={(seconds) => setSecs(seconds)}
+            />
+            <Text style={styles.labelText}>Seconds</Text>
+          </View>
+        </View>
+        <Pressable onPress={submit}>
+          <Text style={styles.button}>SET</Text>
         </Pressable>
       </View>
-    </View>
+    </>
   );
 };
 
---
+++ styles/components/modal.js
@@ -0,0 +1,53 @@
+import { StyleSheet } from "react-native";
+import { vars } from "../variables";
+
+export default styles = StyleSheet.create({
+  button: {
+    color: vars.colors.teal,
+    textAlign: "right",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    gap: 35,
+    zIndex: 2,
+  },
+  selectorContainer: {
+    gap: 20,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  minutesSelectorContainer: {
+    gap: 8,
+    alignItems: "center",
+  },
+  labelText: {
+    color: vars.colors.teal,
+  },
+  overlay: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "blue",
+  },
+  timeInput: {
+    borderColor: vars.colors.teal,
+    borderWidth: 2,
+    width: 80,
+    height: 80,
+    borderRadius: 8,
+    color: vars.colors.teal,
+    fontSize: 45,
+    textAlign: "center",
+  },
+  title: {
+    color: "#fff",
+    paddingLeft: 20,
+    fontSize: 30,
+    fontFamily: vars.fonts.main,
+  },
+});
